95e351b43e6d6c046590a36fa35420d455e40cd7
LanceRadioactive <lancetr.ltr@gmail.com>
2019-07-20 04:21:29 +0300

    New local map generation procedure
    New and improved with hallways no longer branching off into absolutely nowhere

--- js/map/map_local_generate.js
+++ js/map/map_local_generate.js
@@ -1,3 +1,128 @@
+
+function GenerateDungeon(seed){
+  
+  
+  const MAX_HEIGHT = 50
+  const MAX_WIDTH = 50
+  const GEN_CLEAR_THRESHOLD = 0.4
+  const CHECK_PERCENTAGE = 0.9
+  const NUMBER_OF_PASSAGES = 2
+  const ROOM_SIZE_PERCENTAGE = 0.3
+
+  //dist(a)(b) = curried function, returns distance^2 from a to b, assuming a and b formatted as [x, y]
+  const dist = point1 => point2 => ((point1[0]-point2[0])*(point1[0]-point2[0]) + (point1[1]-point2[1])*(point1[1]-point2[1]))
+  //this is a surprise tool that will help us later
+  
+  var height = MAX_HEIGHT
+  var width = MAX_WIDTH
+  var gen = new MersenneTwister(seed)
+  
+  const ROOM_MAX_HEIGHT = Math.floor(MAX_HEIGHT * ROOM_SIZE_PERCENTAGE)
+  const ROOM_MAX_WIDTH = Math.floor(MAX_WIDTH * ROOM_SIZE_PERCENTAGE)
+  
+  
+  var dungeon = new Array(height)
+  for (let index = 0; index < dungeon.length; index++) {
+      dungeon[index] = new Array(width).fill(1);
+  }
+  
+  var rooms = [];
+  while(CheckGeneration(dungeon)<GEN_CLEAR_THRESHOLD){
+    //generate room, respect boundaries
+    //a room is a 4-tuple [top, left, bottom, right]
+    var dim = [Math.floor(gen.random()*ROOM_MAX_HEIGHT)+1, Math.floor(gen.random()*ROOM_MAX_WIDTH)+1]
+    var room = [Math.floor(gen.random()*(height)), Math.floor(gen.random()*(width))]
+    //that describes the topleft corner; add the bottom and right coordinates
+    room.push(Math.min(room[0]+dim[0], height-1))
+    room.push(Math.min(room[1]+dim[1], width-1))
+    
+    //ensure it does not intersect with other rooms, with a 1-C_P chance to overlook
+    var f = false;
+    for (let index = 0; index < rooms.length; index++){
+      if (CheckIntersect(rooms[index], room) && (gen.random()<CHECK_PERCENTAGE)){
+        f = true;
+        break;
+      }
+    }
+    //discard the room if it does, return to beginning
+    if (f) continue;
+    
+    //clear out the space
+    for (let y = room[0]; y <= room[2]; y++){
+      for (let x = room[1]; x <= room[3]; x++){
+        dungeon[y][x] = 0;
+      }
+    }
+    //commit room to memory
+    rooms.push(room)
+    //go anew until enough of the dungeon is cleared out
+  }
+  
+  //rooms cleared! make passages if needed
+  
+  if (NUMBER_OF_PASSAGES <= 0) return dungeon
+  
+  for(var index = 0; index < rooms.length; index++){
+	rooms[index] = [rooms[index][0] + Math.floor(Math.abs(rooms[index][0]-rooms[index][2])/2), rooms[index][1] + Math.floor(Math.abs(rooms[index][1]-rooms[index][3])/2)]
+    //now rooms are [y, x]
+  
+  }
+  
+  
+  console.log(rooms)
+  //for each room - discard it from the list
+  while(rooms.length > 0){
+    var thisRoom = rooms.pop()
+    //pull NUMBER_OF_PASSAGES nearest rooms
+    //remember dist?
+    let distToThis = dist(thisRoom) //distToThis(otherRoom) = dist(thisRoom)(otherRoom)
+    let compare = function(a, b){return Math.sign(distToThis(b) - distToThis(a))} 
+    //^ returns -1/0/1 based on which point is closer
+    //-1 - b is closer; 0 - equal; 1 - a is closer
+    var nearest = [...rooms].sort(compare).slice(-NUMBER_OF_PASSAGES) 
+    //^copy rooms; sort !the copy! by distance in descending order; pick out the last NUMBER_OF_PASSAGES ones 
+    
+    //make passages
+    nearest.forEach(function(thatRoom){
+      if (thisRoom[0] == thatRoom[0]){ //if the rooms are aligned vertically, just make a vertical passage
+        for (var y = Math.min(thisRoom[1], thatRoom[1]);
+            y <= Math.max(thisRoom[1], thatRoom[1]);
+            y++) {
+              dungeon[thisRoom[0]][y] = 0;
+            }
+      }
+      else {
+        //starting x: the one corresponding to lower Y
+        var x1 = thisRoom[0] < thatRoom[0] ? thisRoom[1] : thatRoom[1];
+		//ending x: to higher Y
+		var x2 = thisRoom[0] < thatRoom[0] ? thatRoom[1] : thisRoom[1];
+		var y1 = Math.min(thisRoom[0], thatRoom[0]);
+		var y2 = Math.max(thisRoom[0], thatRoom[0])
+		
+		if (gen.random() > 0.5){
+			for (var index = y1; index <= y2; index ++) {
+				dungeon[index][x1] = 0
+			}
+			for (var index = x1; index <= x2; index ++){
+				dungeon[y2][index] = 0
+			}
+		}
+		else{
+			for (var index = x1; index <= x2; index ++){
+				dungeon[y1][index] = 0
+			}
+			for (var index = y1; index <= y2; index ++) {
+				dungeon[index][x2] = 0
+			}
+		}
+      }
+    })
+  }
+  return dungeon
+  
+}
+
+
 function CheckGeneration(dungeon) { //returns percentage of free space 
     var free = 0;
     dungeon.forEach(function (row) {
@@ -9,89 +134,11 @@ function CheckGeneration(dungeon) { //returns percentage of free space
     return free / (dungeon.length * dungeon[0].length);
 }
 
-function GenerateDungeon(seed) {
-    const MAX_HEIGHT = 50
-    const MAX_WIDTH = 50
-    const GEN_CLEAR_THRESHOLD = 0.3
-
-    const ROOM_MAX_HEIGHT = Math.floor(MAX_HEIGHT * 0.15)
-    const ROOM_MAX_WIDTH = Math.floor(MAX_WIDTH * 0.15)
-
-    var gen = new MersenneTwister(seed)
-    var height = MAX_HEIGHT //<- protect from 0-size errors
-    var width = MAX_WIDTH
-
-    var dungeon = new Array(height);
-
-    for (let index = 0; index < dungeon.length; index++) {
-        dungeon[index] = new Array(width).fill(1);
-    }
-
-    var anchor = [Math.floor(gen.random() * height), Math.floor(gen.random() * width)]
-
-    while (CheckGeneration(dungeon) < GEN_CLEAR_THRESHOLD) {
-        //generate room 
-        var roomHeight = Math.floor(gen.random() * ROOM_MAX_HEIGHT) + 1
-        var roomWidth = Math.floor(gen.random() * ROOM_MAX_WIDTH) + 1
-
-        //generate relative anchor
-        var rel_anchor = [Math.floor(gen.random() * roomHeight), Math.floor(gen.random() * roomWidth)]
-        //clear out room
-        for (var i = 0; i < roomHeight; i++) {
-            for (var j = 0; j < roomWidth; j++) {
-                //clear out a block sized [roomHeight, roomWidth], with the upper left corner being (anchor - rel_anchor), check bounds
-                if (anchor[0] - rel_anchor[0] + i >= 0 && anchor[0] - rel_anchor[0] + i < height && anchor[1] - rel_anchor[1] + j >= 0 && anchor[1] - rel_anchor[1] + j < width) {
-                    dungeon[anchor[0] - rel_anchor[0] + i][anchor[1] - rel_anchor[1] + j] = 0;
-                }
-            }
-        }
-        //choose random free tile as anchor (not sure if rational?)
-        do {
-            var anchor = [Math.floor(gen.random() * height), Math.floor(gen.random() * width)]
-        }
-        while (dungeon[anchor[0]][anchor[1]] == 1)
-
-        //choose direction (as [-1/0/1, -1/0/1])
-        switch (Math.floor(gen.random() * 2)) {
-            case 0:
-                var dir = [Math.floor(gen.random() * 3) - 1, 0]
-                break;
-            case 1:
-                var dir = [0, Math.floor(gen.random() * 3) - 1]
-                break;
-        }
-
-        //choose length of corridor
-        var vert;
-        switch (dir[0]) {
-            case -1:
-                vert = anchor[0];
-                break;
-            case 0:
-                vert = MAX_HEIGHT;
-                break;
-            case 1:
-                vert = height - anchor[0];
-                break;
-        }
-        var hor;
-        switch (dir[1]) {
-            case -1:
-                hor = anchor[1];
-                break;
-            case 0:
-                hor = MAX_WIDTH;
-                break;
-            case 1:
-                hor = width - anchor[1];
-                break;
-        }
-        var len = Math.floor(gen.random() * (Math.min(vert, hor)))
-        //clear out corridor and move anchor to end of corridor
-        for (var i = 0; i < len; i++) {
-            anchor = [anchor[0] + dir[0], anchor[1] + dir[1]]
-            dungeon[anchor[0]][anchor[1]] = 0
-        }
-    }
-    return dungeon
+function CheckIntersect(room1, room2) {
+  //assuming room is a 4-tuple [top, left, bottom, right]
+  
+	var a = ((room1[1]<=room2[3]+1) && (room1[1] >= room2[1]-1)) || ((room2[1]<=room1[3]+1) && (room2[1] >= room1[1]-1))
+	var b = ((room1[0]<=room2[2]+1) && (room1[0] >= room2[0]-1)) || ((room2[0]<=room1[2]+1) && (room2[0] >= room1[0]-1))
+	return a&&b;
+  //returns true IF THEY INTERSECT
 }
