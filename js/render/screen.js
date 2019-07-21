aa81b467532838b0e28f15cd6741e916ae7f858e
Lemis <remminarsettleher@mail.ru>
2019-07-21 20:20:24 +0300

    More useful debug render

--- js/render/screen.js
+++ js/render/screen.js
@@ -1,102 +1,117 @@
 var gDrawingRightNow = false;
 
 function drawScreenSubmap2D(submap) {
-    gDrawingRightNow = true;
-    var fromCoord = [0, 0]; // left-top offset
-    var cellSize = Math.min(width * 0.80 / submap.width, height * 0.80 / submap.height); // px
+	gDrawingRightNow = true;
+	var fromCoord = [0, 0]; // left-top offset
+	var cellSize = Math.min(width * 0.80 / submap.width, height * 0.80 / submap.height); // px
 
-    ctx.lineWidth = 1;
+	ctx.lineWidth = 1;
 
-    for (let iX = 0; iX < submap.width; iX++) {
-        for (let iY = 0; iY < submap.height; iY++) {
-            ctx.strokeStyle = gColorsCGA.WHITE;
-            var cellCoord = [fromCoord[0] + (iY * cellSize), fromCoord[1] + (iX * cellSize)];
+	for (let iX = 0; iX < submap.width; iX++) {
+		for (let iY = 0; iY < submap.height; iY++) {
+			ctx.strokeStyle = gColorsCGA.WHITE;
+			var cellCoord = [fromCoord[0] + (iY * cellSize), fromCoord[1] + (iX * cellSize)];
 
-            ctx.fillStyle = gColorsCGA.BLACK;
+			var text = "";
 
-            var tile = submap.grid[iY][iX];
+			ctx.fillStyle = gColorsCGA.BLACK;
 
-            if (tile.getPass() != true)
-                ctx.fillStyle = gColorsCGA.DARKGRAY;
+			var tile = submap.grid[iY][iX];
 
-            if (tile.items.length > 0)
-                ctx.fillStyle = gColorsCGA.YELLOW;
+			if (tile.getPass() != true)
+				ctx.fillStyle = gColorsCGA.DARKGRAY;
 
-            if (tile.getCreature() != null) {
-                if (tile.getCreature() == gPlayer) {
-                    ctx.fillStyle = gColorsCGA.WHITE;
-                } else
-                    ctx.fillStyle = gColorsCGA.BROWN;
-            }
+			if (tile.items.length > 0) {
+				ctx.fillStyle = gColorsCGA.YELLOW;
+				if (tile.items.length > 9)
+					text = 0;
+				else
+					text = tile.items.length.toString();
+			}
 
-            if (ctx.fillStyle != gColorsCGA.BLACK) {
-                ctx.strokeStyle = ctx.fillStyle;
-                ctx.fillRect(cellCoord[0], cellCoord[1], cellSize, cellSize);
-                ctx.strokeRect(cellCoord[0], cellCoord[1], cellSize, cellSize);
-            }
-            // if (debug_rooms.length > 0) {
-            //     for (var i = 0; i < debug_rooms.length; i++) {
-            //         ctx.strokeStyle = gColorsCGA.RED;
-            //         ctx.strokeRect(debug_rooms[i][1] * cellSize, debug_rooms[i][0] * cellSize, cellSize, cellSize)
-            //     }
-            // }
+			if (tile.getCreature() != null) {
+				if (tile.getCreature() == gPlayer) {
+					ctx.fillStyle = gColorsCGA.WHITE;
+				} else {
+					ctx.fillStyle = gColorsCGA.BROWN;
+					text = tile.getCreature().name.charAt(0);
+				}
+			}
 
-            // if (debug_passages.length > 0) {
-            //     ctx.strokeStyle = gColorsCGA.BLUE;
-            //     for (var i = 0; i < debug_passages.length; i++) {
-            //         ctx.beginPath()
-            //         ctx.moveTo((debug_passages[i][0][1] + 0.5) * cellSize, (debug_passages[i][0][0] + 0.5) * cellSize)
-            //         for (var j = 1; j < debug_passages[i].length; j++) {
-            //             ctx.lineTo((debug_passages[i][j][1] + 0.5) * cellSize, (debug_passages[i][j][0] + 0.5) * cellSize)
-            //         }
-            //         ctx.stroke();
-            //     }
+			if (ctx.fillStyle != gColorsCGA.BLACK) {
+				ctx.fillRect(cellCoord[0], cellCoord[1], cellSize, cellSize);
 
-            // }
-        }
-    }
-    gDrawingRightNow = false;
+				if (tile.getCreature() && tile.getCreature().name) {
+					ctx.textAlign = "center";
+					ctx.font = cellSize * 1.5 + "px Consolas";
+
+					ctx.fillStyle = gColorsCGA.BLACK;
+					ctx.fillText(text, cellCoord[0] + cellSize / 2, cellCoord[1] + cellSize);
+				}
+			}
+			// if (debug_rooms.length > 0) {
+			//     for (var i = 0; i < debug_rooms.length; i++) {
+			//         ctx.strokeStyle = gColorsCGA.RED;
+			//         ctx.strokeRect(debug_rooms[i][1] * cellSize, debug_rooms[i][0] * cellSize, cellSize, cellSize)
+			//     }
+			// }
+
+			// if (debug_passages.length > 0) {
+			//     ctx.strokeStyle = gColorsCGA.BLUE;
+			//     for (var i = 0; i < debug_passages.length; i++) {
+			//         ctx.beginPath()
+			//         ctx.moveTo((debug_passages[i][0][1] + 0.5) * cellSize, (debug_passages[i][0][0] + 0.5) * cellSize)
+			//         for (var j = 1; j < debug_passages[i].length; j++) {
+			//             ctx.lineTo((debug_passages[i][j][1] + 0.5) * cellSize, (debug_passages[i][j][0] + 0.5) * cellSize)
+			//         }
+			//         ctx.stroke();
+			//     }
+
+			// }
+		}
+	}
+	gDrawingRightNow = false;
 }
 
 function drawScreenGlobalmap2D(globalmap) {
-    gDrawingRightNow = true;
-    var fromCoord = [0, 0]; // left-top offset
-    var cellSize = Math.min(width * 0.80 / globalmap.width, height * 0.80 / globalmap.height); // px
+	gDrawingRightNow = true;
+	var fromCoord = [0, 0]; // left-top offset
+	var cellSize = Math.min(width * 0.80 / globalmap.width, height * 0.80 / globalmap.height); // px
 
-    ctx.strokeStyle = gColorsCGA.WHITE;
-    ctx.lineWidth = 1;
+	ctx.strokeStyle = gColorsCGA.WHITE;
+	ctx.lineWidth = 1;
 
-    for (let iX = 0; iX < globalmap.width; iX++) {
-        for (let iY = 0; iY < globalmap.height; iY++) {
-            var cellCoord = [fromCoord[0] + (iY * cellSize), fromCoord[1] + (iX * cellSize)];
+	for (let iX = 0; iX < globalmap.width; iX++) {
+		for (let iY = 0; iY < globalmap.height; iY++) {
+			var cellCoord = [fromCoord[0] + (iY * cellSize), fromCoord[1] + (iX * cellSize)];
 
-            ctx.fillStyle = gColorsCGA.LIGHTGRAY;
+			ctx.fillStyle = gColorsCGA.LIGHTGRAY;
 
-            var tile = globalmap.grid[iY][iX];
+			var tile = globalmap.grid[iY][iX];
 
-            if (tile.onFoot != true) {
-                ctx.fillStyle = gColorsCGA.GREEN; // forest
-                if (tile.onSail == true)
-                    ctx.fillStyle = gColorsCGA.BLUE; // sea
-                if (tile.onFlight != true)
-                    ctx.fillStyle = gColorsCGA.DARKGRAY; // mountain
-            } else if (tile.onSail == true) {
-                ctx.fillStyle = gColorsCGA.LIGHTBLUE; // river
-            }
+			if (tile.onFoot != true) {
+				ctx.fillStyle = gColorsCGA.GREEN; // forest
+				if (tile.onSail == true)
+					ctx.fillStyle = gColorsCGA.BLUE; // sea
+				if (tile.onFlight != true)
+					ctx.fillStyle = gColorsCGA.DARKGRAY; // mountain
+			} else if (tile.onSail == true) {
+				ctx.fillStyle = gColorsCGA.LIGHTBLUE; // river
+			}
 
-            if (tile.submapSeed != null)
-                ctx.fillStyle = gColorsCGA.YELLOW; // submap
+			if (tile.submapSeed != null)
+				ctx.fillStyle = gColorsCGA.YELLOW; // submap
 
-            if (tile.town != null)
-                ctx.fillStyle = gColorsCGA.RED; // town
+			if (tile.town != null)
+				ctx.fillStyle = gColorsCGA.RED; // town
 
-            if (tile.x == gPlayer.globalX && tile.y == gPlayer.globalY) {
-                ctx.fillStyle = gColorsCGA.WHITE; // player
-            }
+			if (tile.x == gPlayer.globalX && tile.y == gPlayer.globalY) {
+				ctx.fillStyle = gColorsCGA.WHITE; // player
+			}
 
-            ctx.fillRect(cellCoord[0], cellCoord[1], cellSize, cellSize)
-            ctx.strokeRect(cellCoord[0], cellCoord[1], cellSize, cellSize);
-        }
-    }
-    gDrawingRightNow = false;
+			ctx.fillRect(cellCoord[0], cellCoord[1], cellSize, cellSize)
+			ctx.strokeRect(cellCoord[0], cellCoord[1], cellSize, cellSize);
+		}
+	}
+	gDrawingRightNow = false;
 }
